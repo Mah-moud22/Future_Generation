0daa67cffbf066e546e2755f73c96630f8aa563e
Mah-moud22 <mah.98.moud.22@gmail.com>
2024-06-12 06:46:33 +0300

    Updates

---
+++ precache-manifest.e254ebfacce8b7a9611a055043e26c9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca6af242f8b2929efb3e3725176d04c",
+    "url": "/Future_Generation/index.html"
+  },
+  {
+    "revision": "278f01a043babea39bfc",
+    "url": "/Future_Generation/static/css/main.482abd90.chunk.css"
+  },
+  {
+    "revision": "f695905856ff9a9a5939",
+    "url": "/Future_Generation/static/js/2.f5c4a8fd.chunk.js"
+  },
+  {
+    "revision": "278f01a043babea39bfc",
+    "url": "/Future_Generation/static/js/main.87bc8721.chunk.js"
+  },
+  {
+    "revision": "3db14fd961b64c6ea178",
+    "url": "/Future_Generation/static/js/runtime~main.27e13f91.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Future_Generation/precache-manifest.e254ebfacce8b7a9611a055043e26c9e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Future_Generation/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
